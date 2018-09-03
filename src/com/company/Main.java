f9302bebfb43bbdcbb2f0b2e0b361a05f896884f
bensonresseler <bresseler.vdab@gmail.com>
2018-09-03 13:41:11 +0200

    Hash-collections - oefn 3, hashmap(key, value) toepassen, opvragen via key, iterable!!!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,76 +1,148 @@
 package com.company;
 
+import java.time.LocalDate;
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        HashMap<String ,BankRekening> bank = new HashMap();
+        List<Product> producten = new ArrayList<>();
+        producten.add(new Product("PLU-1", "gom", 4));
+        producten.add(new Product("PLU-2", "potlood", 2));
+        producten.add(new Product("PLU-3", "lat", 8));
 
-        System.out.println("Rekeningen maken.\n");
-        bank.put("000-0000022-22",new BankRekening("000-0000022-22"));
-        bank.put("000-0000011-11",new BankRekening("000-0000011-11"));
-        bank.put("000-0000033-33",new BankRekening("000-0000033-33"));
-        bank.put("000-0000044-44",new BankRekening("000-0000044-44"));
+        Factuur factuur = new Factuur(LocalDate.now(), "Studio100");
+        factuur.addProduct(producten.get(0));
+        factuur.addProduct(producten.get(1));
+        factuur.addProduct(producten.get(1));
 
-        for(String key: bank.keySet()){
-            System.out.printf("Geef rekeningnummer: %s%n", key);
+        for (Factuurlijn lijn : factuur) {
+            System.out.printf("%s (%d stuks): %d EUR%n", lijn.getProduct(), lijn.getAantal(), lijn.getPrijs());
         }
+        System.out.printf("Totaalprijs: %d EUR%n", factuur.getTotaalPrijs());
 
-        System.out.print("Op welke rekening wil u geld storten? \n");
-        for(String key: bank.keySet()){
-            System.out.printf("%s%n", key);
-        }
-
-        System.out.print("Geef rekeningnummer: ");
-        Scanner scanner =  new Scanner(System.in);
-        String rekStorten = scanner.nextLine();
-
-        System.out.print("Hoeveel wil u storten? ");
-        int bedrag = Integer.parseInt(scanner.nextLine());
-
-        BankRekening r = bank.get(rekStorten);
-        r.storten(bedrag);
-
-        System.out.println("Het resultaat: ");
-        for(String key: bank.keySet()){
-            System.out.printf("%s: %d EUR%n", key, bank.get(key).getSaldo());
-        }
     }
 }
 
-class BankRekening {
-    private String rekeningnummer;
-    private int saldo;
+class Product {
+    private String code;
+    private String omschrijving;
+    private int prijs;
 
-    public BankRekening(String rekeningnummer) {
-        this.rekeningnummer = rekeningnummer;
+    public Product(String code, String omschrijving, int prijs) {
+        this.code = code;
+        this.omschrijving = omschrijving;
+        this.prijs = prijs;
     }
 
-    public String getRekeningnummer() {
-        return rekeningnummer;
+    public String getCode() {
+        return code;
     }
 
-    public int getSaldo() {
-        return saldo;
+    public String getOmschrijving() {
+        return omschrijving;
     }
-    public void storten(int bedrag){
-        saldo += bedrag;
+
+    public int getPrijs() {
+        return prijs;
     }
-    public void afhalen(int bedrag){
-        saldo -= bedrag;
+
+    @Override
+    public String toString() {
+        return omschrijving;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        BankRekening that = (BankRekening) o;
-        return Objects.equals(rekeningnummer, that.rekeningnummer);
+        Product product = (Product) o;
+        return Objects.equals(code, product.code);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(rekeningnummer);
+        return Objects.hash(code);
+    }
+}
+
+class Factuur implements Iterable<Factuurlijn>{
+    private LocalDate datum;
+    private String bedrijf;
+    private HashMap<String, Factuurlijn> factuur = new HashMap<>();
+
+    public Factuur(LocalDate datum, String bedrijf) {
+        this.datum = datum;
+        this.bedrijf = bedrijf;
+    }
+
+    public LocalDate getDatum() {
+        return datum;
+    }
+
+    public String getBedrijf() {
+        return bedrijf;
+    }
+
+    public void addProduct(Product product) {
+         Boolean check = factuur.containsKey(product.getCode());
+        if (check == true) {
+            int aantal = factuur.get(product.getCode()).getAantal() + 1;
+            factuur.get(product.getCode()).setAantal(aantal);
+
+        } else {
+            factuur.put(product.getCode(), new Factuurlijn(product,product.getPrijs(),1));
+        }
+    }
+
+    public int getTotaalPrijs() {
+        int totaal = 0;
+        for (Factuurlijn lijn : factuur.values()){
+            totaal = totaal + lijn.getPrijs();
+        }
+        return totaal;
+    }
+
+    @Override
+    public Iterator<Factuurlijn> iterator() {
+        return factuur.values().iterator();
+    }
+
+
+}
+
+class Factuurlijn {
+    private Product product;
+    private int prijs;
+    private int aantal;
+
+    public Factuurlijn(Product product, int prijs, int aantal) {
+        this.product = product;
+        this.prijs = prijs;
+        this.aantal = aantal;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getPrijs() {
+        return prijs*aantal;
+    }
+
+    public void setPrijs(int prijs) {
+        this.prijs = prijs;
+    }
+
+    public int getAantal() {
+        return aantal;
+    }
+
+    public void setAantal(int aantal) {
+        this.aantal = aantal;
     }
 }
